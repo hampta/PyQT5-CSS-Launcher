4a01068736e17cb2d66ee64dbd73f1ec0b201aac
Artem <56561445+hampta@users.noreply.github.com>
2022-07-14 19:13:58 +0300

    fix build, add requirements

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-python -m nuitka --onefile --output-dir=build --remove-output --prefer-source-code --enable-plugin=anti-bloat --noinclude-pytest-mode=nofollow --noinclude-setuptools-mode=nofollow --lto=yes --jobs=16 --follow-imports --low-memory --nofollow-import-to=PyQt5 --plugin-enable=pyside2 --no-pyi-file ./main.py
+python -m nuitka --onefile --windows-disable-console --output-dir=build --remove-output --prefer-source-code --enable-plugin=anti-bloat --noinclude-pytest-mode=nofollow --noinclude-setuptools-mode=nofollow --lto=yes --jobs=16 --follow-imports --low-memory --nofollow-import-to=PyQt5 --plugin-enable=pyside2 --no-pyi-file ./main.py
 pause
